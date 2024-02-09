dc91b75b0bd4201e544a1f6b4cd37e273dd1cc02
Vlado Bulic <vlado.bulic@hpe.com>
2024-02-09 13:13:21 +0100

    docker compose file added, crud operations added

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var clinet = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = clinet.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
